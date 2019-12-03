bff2002195fa43d88c39b5c16abb02d6227f9ba3
Evheniy Shmyhol <evhen.shev@gmail.com>
2019-12-03 22:10:30 +0200

    task13

---
+++ src/task1/task13/Main.java
@@ -0,0 +1,11 @@
+package task1.task13;
+
+import task1.task13.controller.PyramidController;
+import task1.task13.view.PyramidPrinter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PyramidPrinter.printPyramidForValue(20);
+    }
+}
